b7f917c4a3433e59191a241018ddc07feef8a525
venkateswarareddy555 <pvreddy5858@gmail.com>
2020-10-23 18:19:35 +0530

    Updates

---
+++ precache-manifest.6fb2dc0f29c6dc0ab67ed59a84c55f25.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b8ed3bd76151e61744c21685218af59",
+    "url": "/reactpages/index.html"
+  },
+  {
+    "revision": "e2c7640eb17f69167ddf",
+    "url": "/reactpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6a7f6fea79a5d1db3237",
+    "url": "/reactpages/static/js/2.74710cab.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/reactpages/static/js/2.74710cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2c7640eb17f69167ddf",
+    "url": "/reactpages/static/js/main.15c057f7.chunk.js"
+  },
+  {
+    "revision": "5bf079272d81f2f93928",
+    "url": "/reactpages/static/js/runtime-main.547362c7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
